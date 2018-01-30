b90d26d84b12aae0259197e5725ac4a0262c7e39
Matias Lahore <matiaslahore@gmail.com>
2018-01-30 19:51:18 -0300

    databases access

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,13 @@
 var connection={ 
-	host: 'localhost', 
-	user: 'root',  
-	password: 'root', 
+	host: 'ec2-54-235-249-33.compute-1.amazonaws.com',
+	user: 'stiqzignuwytmv',
+	password: '205b911d326eb481b335c08051b5693714596ae6efa2f3c20cb8fd9da19ecab9',
 	database: 'smart_irrigation',
 };
+// var connection={
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: 'root',
+// 	database: 'smart_irrigation',
+// };
 module.exports = connection;
